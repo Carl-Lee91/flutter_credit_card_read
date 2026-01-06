f655d5a8d8b56e47b6fdb5ee6ade4927ac0bcd8f
Carl Lee <ckdwkdl91@gmail.com>
2026-01-06 11:19:10 +0900

    📦 패키지 수동 추가

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 
 android {
     namespace = "com.example.credit_card_read"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "29.0.14033849"
 
     compileOptions {
@@ -31,7 +31,7 @@ android {
         applicationId = "com.example.credit_card_read"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
